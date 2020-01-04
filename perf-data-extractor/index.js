eabc762bc4dc19806f9bc1d0db4ec43732265809
Bas Bossink <bas.bossink@divverence.com>
2020-01-04 23:49:20 +0100

    Finetune reported timings

--- perf-data-extractor/index.js
+++ perf-data-extractor/index.js
@@ -12,27 +12,35 @@ const puppeteer = require('puppeteer');
   console.log("Gathering performance metrics for", baseUrl);
   await page.goto(baseUrl);
   let pageMetrics = await page.metrics();
-  const performanceTiming = JSON.parse(
-    await page.evaluate(() => JSON.stringify(window.performance.timing))
-  );
   const performanceEntries = JSON.parse(
     await page.evaluate(() => JSON.stringify(window.performance.getEntries()))
   );
-  const resource_entries = performanceEntries.filter(e => e.entryType === 'resource' || e.entryType === 'navigation');
-  const metrics = {
-    numberOfResources: pageMetrics.Documents,
-    transferSize: resource_entries.reduce((acc, curr) => acc + curr.transferSize, 0),
-    encodedBodySize: resource_entries.reduce((acc, curr) => acc + curr.encodedBodySize, 0),
-    decodedBodySize: resource_entries.reduce((acc, curr) => acc + curr.decodedBodySize, 0),
-    backendTime: performanceTiming.responseStart - performanceTiming.navigationStart,
-    timeToFirstByte: performanceTiming.loadEventStart - performanceTiming.navigationStart,
-    timeToStartRender: performanceTiming.domLoading - performanceTiming.navigationStart,
-    timeToInteractive: performanceTiming.domInteractive - performanceTiming.navigationStart,
-    resourceDownloadTime: performanceTiming.responseEnd - performanceTiming.requestStart,
-    dnsLookupTime: performanceTiming.domainLookupEnd -performanceTiming.domainLookupStart
-  }
   let currentTime = Math.floor(Date.now() / 1000);
   await browser.close();
+
+  const resourceEntries = performanceEntries.filter(e => e.entryType === 'resource');
+  const navigationEntry = performanceEntries.filter(e => e.entryType === 'navigation')[0];
+  const earliestRequestStart = Math.min.apply(null, resourceEntries.map(e => e.requestStart));
+  const latestResponseEnd = Math.max.apply(null, resourceEntries.map(e => e.responseEnd));
+  const earliestResponseStart = Math.max.apply(null, resourceEntries.map(e => e.responseStart));
+
+  const metrics = {
+    numberOfResources: pageMetrics.Documents,
+    transferSizeInBytes: resourceEntries.reduce((acc, curr) => acc + curr.transferSize, 0),
+    encodedBodySizeInBytes: resourceEntries.reduce((acc, curr) => acc + curr.encodedBodySize, 0),
+    decodedBodySizeInBytes: resourceEntries.reduce((acc, curr) => acc + curr.decodedBodySize, 0),
+    timeToFirstByteInMilliSeconds: navigationEntry.responseStart - navigationEntry.startTime,
+    timeToStartRenderInMilliSeconds: navigationEntry.domContentLoadedEventStart - navigationEntry.startTime,
+    timeToDomCompleteInMilliSeconds: navigationEntry.domComplete - navigationEntry.startTime,
+    resourceDownloadTimeInMilliSeconds: latestResponseEnd - earliestRequestStart,
+    totalTaskTimeInSeconds: pageMetrics.TaskDuration,
+    dnsLookupTimeInMilliSeconds: navigationEntry.domainLookupEnd - navigationEntry.domainLookupStart,
+    connectionSetupTimeInMilliSeconds: navigationEntry.connectEnd - navigationEntry.connectStart,
+    requestSendPlusResponseLatencyInMilliSeconds: navigationEntry.responseStart - navigationEntry.requestStart,
+    tcpInitiationOverheadInMilliSeconds: navigationEntry.requestStart - navigationEntry.startTime,
+    backendResponseTimeInMilliSeconds: navigationEntry.responseEnd - navigationEntry.responseStart
+  };
+
   console.log("Using", baseUrl, "as base URL for publishing metrics.");
   console.log("Sending metrics:", metrics);
   const instance = axios.create({
